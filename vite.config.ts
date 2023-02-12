229a52281d1b404cd3ae461d03cd07c0893fcad9
lantern <2939954367@qq.com>
2023-02-12 21:42:09 +0800

    更新项目端口

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import {join} from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server:{
+    port: 8848
+  },
   resolve: {
     alias: {
       '@': join(__dirname, "src"),
